73f2f6ec3d5ebc23d9134e8acac5a5cba7a16572
BALAJI POTHULA <balan.pothula@gmail.com>
2020-04-18 11:24:59 +0530

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,15 +1,20 @@
+import sys
+
 try:
   a = int(input("Enter a Value: "))
   b = int(input("Enter b Value: "))
   print("a / b = " + str(a / b))
 except ValueError as e:
   print("system message: " + str(e))
+  print("exception of  : " + str(sys.exc_info()[0]))
   print("user   message: value error")
 except ZeroDivisionError as e:
   print("system message: " + str(e))
+  print("exception of  : " + str(sys.exc_info()[0]))
   print("user   message: zero division error")
 except Exception as e:
   print("system message: " + str(e))
+  print("exception of  : " + str(sys.exc_info()[0]))
   print("user   message: exception")
 finally:
   print("finally block executed")
