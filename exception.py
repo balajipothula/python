c976a773368bff7a624de180c4aa5eaf8e59d06e
BALAJI POTHULA <balan.pothula@gmail.com>
2020-04-18 12:03:44 +0530

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,20 +1,30 @@
 import sys
 
+import inspect
+
+def lineno():
+  """get line number"""
+  return str(inspect.currentframe().f_back.f_lineno)
+  
+def filename():
+  """get file name"""
+  return str(inspect.getframeinfo(inspect.currentframe()).filename)
+
 try:
   a = int(input("Enter a Value: "))
   b = int(input("Enter b Value: "))
   print("a / b = " + str(a / b))
 except ValueError as e:
-  print("system message: " + str(e))
-  print("exception of  : " + str(sys.exc_info()[0]))
+  print("file name: " + filename() + ", line no: " + lineno() + ", system message: " + str(e))
+  print("exception   of: " + str(sys.exc_info()[0]))
   print("user   message: value error")
 except ZeroDivisionError as e:
-  print("system message: " + str(e))
-  print("exception of  : " + str(sys.exc_info()[0]))
+  print("file name: " + filename() + ", line no: " + lineno() + ", system message: " + str(e))
+  print("exception   of: " + str(sys.exc_info()[0]))
   print("user   message: zero division error")
 except Exception as e:
-  print("system message: " + str(e))
-  print("exception of  : " + str(sys.exc_info()[0]))
+  print("file name: " + filename() + ", line no: " + lineno() + ", system message: " + str(e))
+  print("exception   of: " + str(sys.exc_info()[0]))
   print("user   message: exception")
 finally:
   print("finally block executed")
