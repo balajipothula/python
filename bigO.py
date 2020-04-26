2b54ade3f7d7434a19372386a568e0ee9dc57b96
BALAJI POTHULA <balan.pothula@gmail.com>
2020-04-26 12:31:16 +0530

    updated variable names by BALAJI :)

--- bigO.py
+++ bigO.py
@@ -1,7 +1,7 @@
 import time
 import random
 
-e = random.randrange(65536)
+item = random.randrange(65536)
 
 n_array = [n for n in range(65536)]
 
@@ -13,11 +13,11 @@ tail = n_array_len - 1
 start = time.time_ns()
 while head <= tail:
   mid = (head + tail) // 2
-  if n_array[mid] == e:
-    print(e, " found at index ", mid, "execution time ", time.time_ns() - start)
+  if n_array[mid] == item:
+    print(item, " found at index ", mid, "binary search execution time ", time.time_ns() - start)
     break
   else:
-    if e < n_array[mid]:
+    if item < n_array[mid]:
       tail = mid - 1
     else:
       head = mid + 1
@@ -25,6 +25,6 @@ while head <= tail:
 # linier search.
 start = time.time_ns()
 for i in range(n_array_len):
-  if e == n_array[i]:
-    print(e, " found at index ", i,   "execution time ", time.time_ns() - start)
+  if item == n_array[i]:
+    print(item, " found at index ", i,   "linier search execution time ", time.time_ns() - start)
     break
