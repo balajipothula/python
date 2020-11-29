30f1e9f99b968bcd3cabcfe19d018b01add37084
BALAJI POTHULA <balan.pothula@gmail.com>
2020-11-29 12:14:10 +0530

    Update py_virt_env_amzon_linux2.sh

--- orm/py_virt_env_amzon_linux2.sh
+++ orm/py_virt_env_amzon_linux2.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 22 November 2020,
+# Description : Install multiple Python versions.
+
+# https://www.tecmint.com/pyenv-install-and-manage-multiple-python-versions-in-linux/
+
+# update packages repo.
+sudo yum -y update
+
+# upgrading packages.
+sudo yum -y upgrade
+
+# installing epel.
 sudo amazon-linux-extras install epel
 
 sudo yum -y install bzip2-devel       \
@@ -11,12 +26,22 @@ sudo yum -y install bzip2-devel       \
                     sqlite-devel      \
                     zlib-devel
 
+# grabing latest pyenv and pyenv-virtualenv source from github.
+git clone https://github.com/pyenv/pyenv.git             $HOME/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git   $HOME/.pyenv/plugins/pyenv-virtualenv
+
+# setting environment variables.
+echo 'export PYENV_ROOT="$HOME/.pyenv"'    | tee -a "$HOME/.bashrc" "$HOME/.profile" > /dev/null
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' | tee -a "$HOME/.bashrc" "$HOME/.profile" > /dev/null
+
+# enabling shims and autocompletion by adding pyenv init to shell.
+echo 'if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)"; fi' | tee -a "$HOME/.bashrc" "$HOME/.profile" > /dev/null
+
+# starting new shell.
+exec "$SHELL"
+
+# updating python pip setuptools wheel.
 python -m pip install --upgrade pip setuptools wheel
 
-pyenv install 3.6.12
-
-pip install pandas==1.1.1 psycopg2-binary==2.8 -t .
-
-zip -r psycopg2-binary-2.8.zip .
-
-aws s3 cp psycopg2-binary-2.8.zip s3://emp.s3.bucket/psycopg2-binary-2.8.zip
+# installing psycopg specific version.
+pip install psycopg2==2.8.6 -t .
